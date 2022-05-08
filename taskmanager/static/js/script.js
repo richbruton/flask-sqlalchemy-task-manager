b08392db814177b2809ec2df0d640eb6d3abcb62
richbruton <richbruton@gmail.com>
2022-05-08 11:55:04 +0000

    adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialisation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // date picker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
